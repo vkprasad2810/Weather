76be8f16a76c5a097416145a9d9934b9d1b4436a
prasad <prasad@4monks.in>
2024-03-30 16:57:54 +0530

    final Commit

--- app/src/main/java/com/example/weather/di/AuthInterceptor.kt
+++ app/src/main/java/com/example/weather/di/AuthInterceptor.kt
@@ -9,16 +9,12 @@ import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor() : Interceptor {
 
-//    @Inject
-//    lateinit var  preferenceManager : PreferenceManager
+
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val requestBuilder = chain.request().newBuilder()
 
-        // If token has been saved, add it to the request
-//        preferenceManager.getAccessToken().let {
-//            requestBuilder.addHeader("Authorization", "Bearer $it")
-//        }
+
 
         return chain.proceed(requestBuilder.build())
     }
